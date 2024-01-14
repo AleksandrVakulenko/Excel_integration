e61cdfccc105a47d44d206d0cddf7b6b1a098a0d
Aleksandr Vakulenko <sasha705@mail.ru>
2024-01-14 16:55:10 +0300

    DBTable filtering function

--- DBTable.m
+++ DBTable.m
@@ -5,14 +5,19 @@ classdef DBTable < handle
     
     methods (Access = public)
         function obj = DBTable(filename, varargin)
-            narginchk(1, 2);
-            C = readcell(filename, 'Sheet', 'Основная');
+            narginchk(1, 3);
+            if nargin > 1
+                Sheetname = char(varargin{1});
+            else
+                Sheetname = '';
+            end
+            C = readcell(filename, 'Sheet', Sheetname);
             Header = C(1,:);
             obj.Header = string(Header)';
-            obj.Table = readtable(filename, 'Sheet', 'Основная', ...
+            obj.Table = readtable(filename, 'Sheet', Sheetname, ...
                 'VariableNamingRule', 'preserve');
-            if nargin == 2
-                Columns = varargin{1};
+            if nargin > 2
+                Columns = varargin{2};
                 Columns = reshape(Columns, [1, numel(Columns)]);
                 indexes = (obj.Header == Columns);
                 indexes = (sum(indexes, 2) > 0);
@@ -28,6 +33,23 @@ classdef DBTable < handle
         end
 
         function Unique = get_unique(obj, Column)
+            col_ind = obj.find_column(Column);
+            Unique = unique(obj.Table{:, col_ind});
+            Unique = string(Unique);
+        end
+
+        function Table = filter(obj, filter_obj)
+            temp_pbj = obj;
+            col_ind = temp_pbj.find_column(filter_obj.header_name);
+            Table = temp_pbj.Table;
+            Column = Table(:, col_ind);
+            range = table2cell(Column) == filter_obj.value;
+            Table(~range, :) = [];
+        end
+
+        % find column index by name or just passes input index
+        % fixme: add range check
+        function col_ind = find_column(obj, Column)
             if isnumeric(Column) && numel(Column) == 1
                 col_ind = Column;
             elseif isnumeric(Column) && numel(Column) ~= 1
@@ -35,8 +57,6 @@ classdef DBTable < handle
             else
                 col_ind = find(obj.Header == string(Column));
             end
-            Unique = unique(obj.Table{:, col_ind});
-            Unique = string(Unique);
         end
 
 
---
+++ test.m
@@ -0,0 +1,26 @@
+
+
+clc
+
+
+Columns = ["№солонца", "Полная дата", "Пол", "Возраст", ...
+    "Продолжительность посещения, мин"];
+MooseDB = DBTable("Moose1.xlsx", 'Основная', Columns);
+
+% PhotoDB = DBTable("Photo1.xlsx", 'Основная');
+
+
+%%
+
+clc
+
+MooseDB.get_header
+MooseDB.get_unique("№солонца")
+
+
+filter_obj.header_name = "№солонца";
+filter_obj.value = "1.1.";
+
+Table = MooseDB.filter(filter_obj);
+
+
