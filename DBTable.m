9ed5d78b4c95be24ef6b555e69d34cc4f0397899
Aleksandr Vakulenko <sasha705@mail.ru>
2024-01-15 21:32:01 +0300

    update

--- DBTable.m
+++ DBTable.m
@@ -4,6 +4,9 @@
 classdef DBTable < handle
     
     methods (Access = public)
+        % create DBTable by opening a file
+        % 2nd arg is a sheetname of excel
+        % erd arg is a name of columns to save
         function obj = DBTable(filename, varargin)
             narginchk(1, 3);
             if nargin > 1
@@ -12,7 +15,7 @@ classdef DBTable < handle
                 Sheetname = '';
             end
             C = readcell(filename, 'Sheet', Sheetname);
-            Header = C(1,:);
+            Header = C(1, :);
             obj.Header = string(Header)';
             obj.Table = readtable(filename, 'Sheet', Sheetname, ...
                 'VariableNamingRule', 'preserve');
@@ -21,23 +24,28 @@ classdef DBTable < handle
                 Columns = reshape(Columns, [1, numel(Columns)]);
                 indexes = (obj.Header == Columns);
                 indexes = (sum(indexes, 2) > 0);
-                rev_indexes = logical(1 - indexes);
-                obj.Header(rev_indexes) = [];
-                obj.Table(:, rev_indexes) = [];
+                inv_indexes = logical(1 - indexes);
+                obj.Header(inv_indexes) = [];
+                obj.Table(:, inv_indexes) = [];
             end
             disp(['DBTable loaded from file "' char(filename) '"'])
         end
-
+    
+        % get all of column names
         function Header = get_header(obj)
             Header = obj.Header;
         end
 
+        % returns unique values of <Column> content
+        % <Column> is an integer value of column (>=1, <=size)
+        % OR <Column> is a string (or char array) name
         function Unique = get_unique(obj, Column)
             col_ind = obj.find_column(Column);
             Unique = unique(obj.Table{:, col_ind});
             Unique = string(Unique);
         end
 
+        % produces a new table by filtering current
         function Table = filter(obj, filter_obj)
             temp_pbj = obj;
             col_ind = temp_pbj.find_column(filter_obj.header_name);
@@ -48,14 +56,22 @@ classdef DBTable < handle
         end
 
         % find column index by name or just passes input index
-        % fixme: add range check
         function col_ind = find_column(obj, Column)
             if isnumeric(Column) && numel(Column) == 1
                 col_ind = Column;
             elseif isnumeric(Column) && numel(Column) ~= 1
-                error('err placeholder'); % fixme
+                error('error placeholder'); % fixme
             else
                 col_ind = find(obj.Header == string(Column));
+            end
+            if isempty(col_ind)
+                error('no such column')
+            end
+            if col_ind < 1
+                error('column index must be >= 1')
+            end
+            if col_ind > numel(obj.Header)
+                error(['column index must be <= ' num2str(numel(obj.Header))])
             end
         end
 
--- test.m
+++ test.m
@@ -10,6 +10,32 @@ MooseDB = DBTable("Moose1.xlsx", 'Основная', Columns);
 % PhotoDB = DBTable("Photo1.xlsx", 'Основная');
 
 
+%% copy and reference differences:
+clc
+A = 3 % init A
+B = A % copy A to B
+% now B is copy of A
+% B is a same class as A
+% B has a same contens as A
+% B is euqal to A
+A == B % it is logical true
+
+% if we do copy of DBTable
+MooseDB2 = MooseDB;
+% and check equality
+MooseDB2 == MooseDB
+% it is also logical true
+% but in a different way
+% now it means that MooseDB and MooseDB2 referencing the same object
+% if we modify one of them
+% it modifies all other "copy"
+% in programming such variabels are called "reference to ..."
+% in our case it is reference to DBTable object
+
+% copy
+Table = MooseDB.filter(filter_obj);
+
+
 %%
 
 clc
@@ -22,5 +48,28 @@ filter_obj.header_name = "№солонца";
 filter_obj.value = "1.1.";
 
 Table = MooseDB.filter(filter_obj);
+
+
+%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
